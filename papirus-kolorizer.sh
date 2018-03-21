77fb99df47f9fd3cc716d721cc104bddc4a077f2
DarthWound <darthwound@live.com>
2018-03-21 23:16:06 +0100

    Add files via upload

---
+++ papirus-kolorizer.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Change Papirus folders color
+
+# 1- Go to https://www.w3schools.com/colors/colors_picker.asp
+# 2- Enter the color of your choice in the picker
+# 3- Refer to the "lighter/darker" table
+# 3- Use color-10% for "Back" and color-30% for "Symbol"
+readonly FrontColor="7952B3"
+readonly BackColor="5E3E8E"
+readonly SymbolColor="2F1F47"
+
+### Some colors
+## Material Design palette -> https://material.io/guidelines/style/color.html#color-color-palette
+## Debian red = D70A53
+## Fedora blue = 3C6EB4
+## openSUSE lightgreen = 73BA25
+## openSUSE darkgreen = 6DA741
+## openSUSE lightcyan = 35B9AB
+## openSUSE darkcyan = 00A489
+## RedHat red = CC0000
+## RedHat lightblue = A3DBE8
+## RedHat darkblue = 004153
+## SUSE green = 02D35F
+## Ubuntu orange = E95420
+## Ubuntu purple = 772953
+## Budgie slate = 404552
+## GNOME Adwaita blue = 4A90D9
+## KDE Plasma blue = 3DAEE9
+## Papirus black = 505050
+## Papirus grey = 8D8D8D
+## Papirus brown = AE8D6E
+## Papirus green = 84B05F
+## Papirus teal = 009F85
+## Papirus cyan = 00BAD2
+## Papirus blue = 4F92DE
+## Bootstrap purple = 7952B3
+## Bulma green = 00D1B2
+## KNACSS green = 13747D
+## KNACSS red = CF361B
+## Lamborghini gold = DDB321
+## Mozilla beige = D7D3C8
+## OnePlus red = EB0029
+## Pantone ultraviolet = 5F4B8B
+## Pantone rose quartz = F7CAC9
+## Pantone serenity = 92A8D1
+## Sherwin taupe = 8C7E78
+## Gunmetal = 2C3539
+## DarthWound red = A63F3F
+
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sh
+
+sleep 2s
+
+echo "Changing colors..."
+
+sudo find /usr/share/icons/Papirus/*/places -type f -name "*.svg" -exec sed -i "s/5294e2/$FrontColor/gI" {} \;
+sudo find /usr/share/icons/Papirus/*/places -type f -name "*.svg" -exec sed -i "s/4877b1/$BackColor/gI" {} \;
+sudo find /usr/share/icons/Papirus/*/places -type f -name "*.svg" -exec sed -i "s/1d344f/$SymbolColor/gI" {} \;
+
+clear
+printf "Kolorized! You can change your icon theme now.\nRestarting your session may be necessary.\n"
+read -p "Press ENTER to close."
